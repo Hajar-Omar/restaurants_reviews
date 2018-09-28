f3f0bf1a745833ba9da62d8b541e50143b41c862
HajarOmar <hajaromar280@yahoo.com>
2018-09-28 18:37:16 +0200

    make the site offline first

---
+++ sw.js
@@ -0,0 +1,65 @@
+   var CACHE_NAME ='restaurant-cache-v12';
+    var urlsToCache = [
+        '/',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/index.html',
+        'restaurant.html'
+    ];
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache){
+            return cache.addAll(
+                urlsToCache
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+  });
+}
+//caches.delete('restaurant-cache-v10');
+
+
+// self.addEventListener('fetch', function(event) {
+// console.log(event.request);
+// });
